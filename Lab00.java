4029e884e78bc074dd9f4a094a9b91cf46ba32e3
manug16 <manu@ManuMacBook.local>
2014-10-24 08:29:59 +0100

    Lab00

--- Lab00.java
+++ Lab00.java
@@ -11,8 +11,8 @@ import edu.fcps.karel2.Robot;
 
 public class Lab00 {
 
-     public static void main(String[] args) {
-             Display.openWorld("maps/first.map");
+   public static void main(String[] args) {
+   Display.openWorld("maps/first.map");
    Display.setSize(10, 10);
    Display.setSpeed(3);
    Robot karel = new Robot();
